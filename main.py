aec5dae953e2b25584b8aa2f9e5426ffc10593b2
citizenccc <v.pisarev69@gmail.com>
2022-02-08 23:35:22 +0600

    Update main.py

--- main.py
+++ main.py
@@ -7,32 +7,17 @@
 # s = 'spameggs'
 # s[3:5]
 
-# s[:] - полностью
-# s[::-1] - полностью переворачивает
-
-# string[start:stop:step]
-
-
-
-# name = input('Enter your name: ')
-# surname = input('Enter your surname: ')
-# year_of_birth = int(input('Your year of birth: '))
-
-# message = 'Hello ' + name + ' ' + surname + ", you're  " + str((2021 - year_of_birth))
+# s[:] - полностьcюSDF= 'Hello ' + name + ' ' + surname + ", you're  " + str((2021 - year_of_birth))
 # message_with_format = f'hello {name} {surname} you\'re {2021 - year_of_birth}!'
-# print(message_with_format)
-
-# age = int(input('Your age: '))
-
-# if age >= 18:
-#     print("Welcome!")
-# else:
-#     print("fuck off")
-
-
-# some_str = 'Hello world!'
-# for symb in some_str:
-#     print(symb)
-
-
-#https://pythonworld.ru/
+# print(message_with_format)XcXc
+#     print("fuck off")X
+# some_str = 'Hello world!'cX in some_str:
+#     prinFS
+JGYS
+AES
+SFCS
+C
+Ad
+E
+WR
+EWT
