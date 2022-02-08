0b4cc9cfe8e471ba37ebbadb887b87e101afcda7
citizenccc <v.pisarev69@gmail.com>
2022-02-08 23:30:26 +0600

    Second lecture

---
+++ main.py
@@ -0,0 +1,38 @@
+# print('John'[0])
+
+# [-0] - не бывает, в обратном порядке начинается с -1
+
+# срезы start/stop/step 
+# start входит, stop не входит
+# s = 'spameggs'
+# s[3:5]
+
+# s[:] - полностью
+# s[::-1] - полностью переворачивает
+
+# string[start:stop:step]
+
+
+
+# name = input('Enter your name: ')
+# surname = input('Enter your surname: ')
+# year_of_birth = int(input('Your year of birth: '))
+
+# message = 'Hello ' + name + ' ' + surname + ", you're  " + str((2021 - year_of_birth))
+# message_with_format = f'hello {name} {surname} you\'re {2021 - year_of_birth}!'
+# print(message_with_format)
+
+# age = int(input('Your age: '))
+
+# if age >= 18:
+#     print("Welcome!")
+# else:
+#     print("fuck off")
+
+
+# some_str = 'Hello world!'
+# for symb in some_str:
+#     print(symb)
+
+
+#https://pythonworld.ru/
